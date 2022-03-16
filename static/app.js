6fe1131b8e225c936fa3df7598ccfa6e10e3b580
victor augusto <1augusto.victor@gmail.com>
2022-03-16 01:55:49 -0300

    Change the way results are being displayed

--- static/app.js
+++ static/app.js
@@ -1,25 +1,37 @@
 const Controller = {
   search: (ev) => {
-    ev.preventDefault();
-    const form = document.getElementById("form");
-    const data = Object.fromEntries(new FormData(form));
+    ev.preventDefault()
+    const form = document.getElementById("form")
+    const data = Object.fromEntries(new FormData(form))
     const response = fetch(`/search?q=${data.query}`).then((response) => {
       response.json().then((results) => {
-        Controller.updateTable(results);
+        Controller.updateHtml(results)
+        Controller.highlightResults(data.query)
       });
     });
   },
 
-  updateTable: (results) => {
-    const table = document.getElementById("table-body");
-    const rows = [];
-    for (let result of results) {
-      highlightedResult = result.replace(/hamlet/gi, "<mark>$&</mark>")
-      rows.push(`<tr>${highlightedResult}<tr/>`);
-    }
-    table.innerHTML = rows;
+  updateHtml: (results) => {
+    document.getElementById("results").innerHTML = ''
+    results.forEach(function (result, i) {
+      const node = document.createElement("pre")
+      const rule = document.createElement("hr")
+      const textNode = document.createTextNode(result)
+      node.appendChild(textNode)
+      document.getElementById("results").appendChild(node)
+      document.getElementById("results").appendChild(rule)
+    })
   },
+
+  highlightResults: (query) => {
+    const rawResults = document.querySelectorAll("pre")
+    const matcher = new RegExp(query, "gi")
+    rawResults.forEach( function(element) {
+        element.innerHTML = element.innerHTML.replace(matcher, "<mark>$&</mark>")
+      }
+    )
+  }
 };
 
-const form = document.getElementById("form");
-form.addEventListener("submit", Controller.search);
+const form = document.getElementById("form")
+form.addEventListener("submit", Controller.search)
